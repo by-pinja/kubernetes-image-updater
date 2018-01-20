0398a2d795254d0a16bfd2f877965d91e8246432
Savpek <savolainen.pekka@gmail.com>
2018-01-20 09:36:35 +0200

    Added more scenarios.

--- Domain/ImageUpdater.cs
+++ Domain/ImageUpdater.cs
@@ -16,6 +16,30 @@ namespace Updater.Domain
             _logger = logger;
         }
 
+        private class ImageRow
+        {
+            private ImageRow() {}
+            public string Image { get; private set; }
+            public string ContainerName { get; private set; }
+            public string DeploymentName { get; private set; }
+            public string NameSpace { get; private set; }
+
+            public static ImageRow Create(JToken container, JToken deployment)
+            {
+                return new ImageRow
+                {
+                    Image = container["image"].Value<string>() ??
+                        throw new InvalidOperationException($"Missing containers->image"),
+                    ContainerName = container["name"].Value<string>() ??
+                        throw new InvalidOperationException($"Missing containers->name"),
+                    DeploymentName = deployment["metadata"]["name"].Value<string>() ??
+                        throw new InvalidOperationException($"Missing metadata->name"),
+                    NameSpace = deployment["metadata"]["namespace"].Value<string>() ??
+                        throw new InvalidOperationException($"Missing metadata->namespace")
+                };
+            }
+        }
+
         public void UpdateEventHandler(string imageUri)
         {
             var parsedUri = ImageUriParser.ParseUri(imageUri);
@@ -33,17 +57,7 @@ namespace Updater.Domain
                             var containers = deployment["spec"]["template"]["spec"]["containers"].Value<JArray>();
 
                             return containers
-                                .Select(container => new
-                                {
-                                    Image = container["image"].Value<string>() ??
-                                        throw new InvalidOperationException($"Missing containers->image"),
-                                    ContainerName = container["name"].Value<string>() ??
-                                        throw new InvalidOperationException($"Missing containers->name"),
-                                    DeplymentName = deployment["metadata"]["name"].Value<string>() ??
-                                        throw new InvalidOperationException($"Missing metadata->name"),
-                                    NameSpace = deployment["metadata"]["namespace"].Value<string>() ??
-                                        throw new InvalidOperationException($"Missing metadata->namespace")
-                                });
+                                .Select(container => ImageRow.Create(container, deployment));
                         })
                         .ToList();
 
@@ -51,7 +65,7 @@ namespace Updater.Domain
                         .Where(image => ImageUriParser.ParseUri(image.Image).uri == parsedUri.uri).ToList()
                         .ForEach(image =>
                         {
-                            _shell.Run($"kubectl set image deployment/{image.DeplymentName} {image.ContainerName}={imageUri} --namespace={image.NameSpace}");
+                            _shell.Run($"kubectl set image deployment/{image.DeploymentName} {image.ContainerName}={imageUri} --namespace={image.NameSpace}");
                         });
                 }, error => {
                     _logger.LogError($"Failed to fetch deployment json from kubernetes, error: {error}");
--- Domain/ImageUpdaterTests.cs
+++ Domain/ImageUpdaterTests.cs
@@ -51,5 +51,23 @@ namespace Updater.Domain
             shell.DidNotReceive().Run(Arg.Is<string>(cmd => cmd.Contains("kubectl set image")));
             logger.Received(1).LogError(Arg.Any<string>());
         }
+
+        [Fact]
+        public void WhenUpdingImageReturnsError_ThenLogIt()
+        {
+            var shell = Substitute.For<ICommandLine>();
+            var logger = Substitute.For<ILogger<ImageUpdater>>();
+            var updater = new ImageUpdater(shell, logger);
+
+            shell.Run("kubectl get deployments --all-namespaces -o json")
+                .Returns(TestPathUtil.GetTestDataContent("realdata.json"));
+
+            shell.Run(Arg.Is<string>(cmd => cmd.Contains("kubectl set image")))
+                .Returns(new InvalidOperationException());
+
+            updater.UpdateEventHandler("eu.gcr.io/ptcs-docker-registry/authorization:123-master");
+
+            logger.Received(1).LogError(Arg.Any<string>());
+        }
     }
 }
